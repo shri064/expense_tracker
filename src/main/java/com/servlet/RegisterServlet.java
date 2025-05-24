ed92dc24852d6f1e59b2b7b34d63db7d7ba63c95
shri064 <111745722+shri064@users.noreply.github.com>
2025-05-25 00:06:50 +0530

    Update RegisterServlet.java

--- src/main/java/com/servlet/RegisterServlet.java
+++ src/main/java/com/servlet/RegisterServlet.java
@@ -1,46 +1,43 @@
 package com.servlet;
 
 import java.io.IOException;
-
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 
 import com.dao.UserDao;
 import com.db.HibernateUtil;
 import com.entity.User;
 
 @WebServlet("/userRegister")
-public class RegisterServlet extends HttpServlet{
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		String name = req.getParameter("fullName");
-		String email = req.getParameter("email");
-		String password = req.getParameter("password");
-		String about = req.getParameter("about");
-		
-		User u = new User(name, email, password, about);
-		
-		UserDao dao = new UserDao(HibernateUtil.getEntityManagerFactory());
-		boolean f = dao.saveUser(u);
-		
-		HttpSession session = req.getSession();
-		
-		if(f) {
-			session.setAttribute("msg", "Register Successfully");
-			resp.sendRedirect("register.jsp");
-		}
-		else {
-			session.setAttribute("msg", "Something Went Wrong");
-			resp.sendRedirect("register.jsp");
-		}
-	}
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("fullName");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String about = req.getParameter("about");
+
+        HttpSession session = req.getSession();
+        UserDao dao = new UserDao(HibernateUtil.getEntityManagerFactory());
+
+        
+        User existingUser = dao.getUserByEmail(email);
+        if (existingUser != null) {
+            session.setAttribute("msg", "User already exists");
+            resp.sendRedirect("register.jsp");
+            return;
+        }
+
+        User u = new User(name, email, password, about);
+        boolean f = dao.saveUser(u);
+
+        if (f) {
+            session.setAttribute("msg", "Register Successfully");
+        } else {
+            session.setAttribute("msg", "Something Went Wrong");
+        }
+
+        resp.sendRedirect("register.jsp");
+    }
 }
-
-
-//  1.01
